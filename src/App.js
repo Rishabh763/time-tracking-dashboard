11d8daeb24db30e755a5f51be1b2f37a7d0c8b52
Rishabh763 <rs0284388@gmail.com>
2024-02-12 00:54:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <main>
+        <div className='profile'>
+          
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
